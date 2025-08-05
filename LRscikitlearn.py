327017c842114f0ea71ba6a8bba647585f2ec2f5
shrutibajpai23 <shrutigithub1@gmail.com>
2025-08-05 21:29:28 +0530

    Add files via upload

--- LRscikitlearn.py
+++ LRscikitlearn.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
+import matplotlib.pyplot as plt 
 
 #generate synthetic data 
 np.random.seed(42)
@@ -22,4 +23,18 @@ y_pred=model.predict(x_test)
 #print coeffiecients 
 print("slope: ",model.coef_[0][0])
 print("intercept: ",model.intercept_[0])
- 
+
+
+#visualize 
+plt.scatter(x_test,y_test,color='blue',label="testing data")
+plt.plot(x_test,y_pred,color='red',label="predicted line")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("linear regression")
+
+#evaluate model 
+mse=mean_squared_error(y_test,y_pred)
+r2=r2_score(y_test,y_pred)
+print("MSE: ", mse)
+print("R^2: ", r2)
+plt.legend()
